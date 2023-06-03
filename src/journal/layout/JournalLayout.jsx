1bbd38a95d894242dbf4663edf1c2ec11e8d688c
Henry David Yamunaque Villegas <david_yamunaque_5@outlook.com>
2023-06-02 23:34:48 -0500

    feat: theme, components, views and pages

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,22 @@
+import { Box, Toolbar } from "@mui/material";
+import { Navbar, Sidebar } from "../components";
+
+const drawerWidth = 240;
+
+export const JournalLayout = ({ children }) => {
+  return (
+    <Box sx={{ display: "flex" }}>
+      {/* Navbar */}
+      <Navbar drawerWidth={drawerWidth} />
+
+      {/* Sidebar */}
+      <Sidebar drawerWidth={drawerWidth} />
+
+      <Box component="main" sx={{ flexGrow: 1, padding: 3 }}>
+        {/* Toolbar */}
+        <Toolbar />
+        {children}
+      </Box>
+    </Box>
+  );
+};
---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,31 @@
+import { IconButton, Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
+import { NoteView } from "../views";
+import { NothingSelectedView } from "../views";
+import { AddOutlined } from "@mui/icons-material";
+
+export const JournalPage = () => {
+  return (
+    <JournalLayout>
+      {/* <Typography>Journal Page</Typography> */}
+      {/* NothinSelected */}
+      {/* NoteView */}
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hober": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
+    </JournalLayout>
+  );
+};
